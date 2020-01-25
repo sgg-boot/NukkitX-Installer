7f7f3e872f892fd094b2efb24354e62828f62639
Siter <60112146+sgg-boot@users.noreply.github.com>
2020-01-25 11:05:06 -0300

    Installer File
    Installer File

---
+++ installer.sh
@@ -0,0 +1,25 @@
+﻿#!/bin/bash
+  apt install openjdk-8-jdk openjdk-8-jre wget
+  apt install default-jdk default-jre wget
+    
+    pacman -S jdk8-openjdk jre-openjdk wget
+      
+      rm -rf *
+        
+        wget -O nukkit.jar https://ci.nukkitx.com/job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
+          
+          echo '#!/bin/bash' >> run.sh
+          echo 'echo 'Starting The Server'' >> run.sh
+          echo 'java -Xmx1024M -Xms256M -jar nukkit.jar' >> run.sh
+          
+          echo '#!/bin/bash' >> update.sh
+          echo 'echo 'Starting The Update'' >> update.sh
+          echo 'rm -rf nukkit.jar' >> update.sh
+          echo 'wget -O nukkit.jar https://ci.nukkitx.com/job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar' >> update.sh
+          echo 'echo 'Update Complete'' >> update.sh
+            
+            cd
+              
+              mv Ansi NukkitX
+          
+          echo 'Use The cd ../NukkitX && ./run.sh'           
